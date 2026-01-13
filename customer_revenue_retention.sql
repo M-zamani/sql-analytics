3f80891700cc5e18445a9e1965f0ab0ed6b867cb
Mahmoud-Zamani <95590186+M-zamani@users.noreply.github.com>
2026-01-13 10:18:53 -0500

    Update customer_revenue_retention.sql

--- customer_revenue_retention.sql
+++ customer_revenue_retention.sql
@@ -1,1 +1,11 @@
+-- Customer Revenue & Retention Analysis
+-- Author: Mahmoud Zamani
+-- Purpose: Analyze customer behavior, revenue, and retention trends
+/*
+Business Questions:
+1. How does revenue change month over month?
+2. Who are the highest-value customers?
+3. How many customers are active each month?
+4. How does revenue vary by customer cohort?
+*/
 
